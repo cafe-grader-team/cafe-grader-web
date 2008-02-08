d9777e561bf7a54473fd938bb3f3e513237c4fef
jittat <jittat@6386c4cd-e34a-4fa8-8920-d93eb39b512e>
2008-02-08 12:26:56 +0000

    small changes, layouts
    git-svn-id: http://theory.cpe.ku.ac.th/grader/web/trunk@46 6386c4cd-e34a-4fa8-8920-d93eb39b512e

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
 
-  def user_options
+  def user_header
     options = ''
     user = User.find(session[:user_id])
     if user.admin? 
