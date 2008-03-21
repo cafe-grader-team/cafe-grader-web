3ba56a77990ffcaa0aafe9858816a5d76dcc5e6a
jittat <jittat@6386c4cd-e34a-4fa8-8920-d93eb39b512e>
2008-03-21 17:05:08 +0000

    [web] fixing ticket #10
    git-svn-id: http://theory.cpe.ku.ac.th/grader/web/trunk@142 6386c4cd-e34a-4fa8-8920-d93eb39b512e

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -31,4 +31,14 @@ module ApplicationHelper
                                      :action => action)
   end
 
+  def format_short_time(time)
+    now = Time.now
+    st = ''
+    if (time.yday != now.yday) or
+	(time.year != now.year)
+      st = time.strftime("%x ")
+    end
+    st + time.strftime("%X")
+  end
+
 end
--- app/helpers/main_helper.rb
+++ app/helpers/main_helper.rb
@@ -1,13 +1,3 @@
 module MainHelper
 
-  def format_short_time(time)
-    now = Time.now
-    st = ''
-    if (time.yday != now.yday) or
-	(time.year != now.year)
-      st = time.strftime("%x ")
-    end
-    st + time.strftime("%X")
-  end
-
 end
