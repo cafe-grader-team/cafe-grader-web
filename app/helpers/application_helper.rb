c0a621925f2d2f2b2aca661d857ae8cb63a71096
jittat <jittat@6386c4cd-e34a-4fa8-8920-d93eb39b512e>
2008-03-31 16:56:23 +0000

    [web] small styling, split out admin's menu, put config into table.info
    git-svn-id: http://theory.cpe.ku.ac.th/grader/web/trunk@157 6386c4cd-e34a-4fa8-8920-d93eb39b512e

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,25 +5,22 @@ module ApplicationHelper
     menu_items = ''
     user = User.find(session[:user_id])
 
+    if (user!=nil) and (user.admin?) 
+      # admin menu
+      menu_items << "<b>Administrative task:</b> "
+      append_to menu_items, '[Problem admin]', 'problems', 'index'
+      append_to menu_items, '[User admin]', 'user_admin', 'index'
+      append_to menu_items, '[User stat]', 'user_admin', 'user_stat'
+      #append_to menu_items, '[Graders]', 'graders', 'list'
+      append_to menu_items, '[Site config]', 'configurations', 'index'
+      menu_items << "<br/>"
+    end
+
     # main page
     append_to menu_items, '[Main]', 'main', 'list'
     append_to menu_items, '[Submissions]', 'main', 'submission'
     append_to menu_items, '[Test]', 'test', 'index'
-
-    # admin menu
-    if (user!=nil) and (user.admin?) 
-      append_to menu_items, '[Problem admin]', 'problems', 'index'
-      append_to menu_items, '[User admin]', 'user_admin', 'index'
-      append_to menu_items, '[User stat]', 'user_admin', 'user_stat'
-    end
-
-    # general options
     append_to menu_items, '[Settings]', 'users', 'index'
-
-    if (user!=nil) and (user.admin?) 
-      append_to menu_items, '[Site config]', 'configurations', 'index'
-    end
-
     append_to menu_items, '[Log out]', 'main', 'login'
 
     menu_items
