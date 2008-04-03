117069e00fe768f5e1d9fc22d82722521eb63063
jittat <jittat@6386c4cd-e34a-4fa8-8920-d93eb39b512e>
2008-04-03 19:34:13 +0000

    [web] more files (cont. from rev 159)
    git-svn-id: http://theory.cpe.ku.ac.th/grader/web/trunk@160 6386c4cd-e34a-4fa8-8920-d93eb39b512e

---
+++ spec/controllers/test_controller_spec.rb
@@ -0,0 +1,28 @@
+
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe TestController do
+
+  before(:each) do
+    @john = mock(User, :id => "1", :login => 'john')
+    @john_result = mock(TestRequest, :id => "1", :user_id => @john.id)
+    @mary_result = mock(TestRequest, :id => "2", :user_id => @john.id + '1')
+    User.should_receive(:find).with(@john.id).and_return(@john)
+  end
+
+  it "should let user see her testing result" do
+    TestRequest.should_receive(:find).with(@john_result.id).
+      and_return(@john_result)
+    get 'result', {:id => @john_result.id}, {:user_id => @john.id}
+    response.should be_success
+  end
+
+  it "should not let user see other's testing result" do
+    TestRequest.should_receive(:find).with(@mary_result.id).
+      and_return(@mary_result)
+    get 'result', {:id => @mary_result.id}, {:user_id => @john.id}
+    response.should redirect_to(:action => 'index')
+  end
+  
+end
+
