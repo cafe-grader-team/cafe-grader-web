7dc9347ef53a81575157a69036eaf2c539e66436
jittat <jittat@6386c4cd-e34a-4fa8-8920-d93eb39b512e>
2008-04-17 15:51:28 +0000

    [web] forgot migration
    git-svn-id: http://theory.cpe.ku.ac.th/grader/web/trunk@182 6386c4cd-e34a-4fa8-8920-d93eb39b512e

---
+++ db/migrate/029_add_test_allowed_to_problems.rb
@@ -0,0 +1,15 @@
+class AddTestAllowedToProblems < ActiveRecord::Migration
+  def self.up
+    add_column :problems, :test_allowed, :boolean
+    Problem.reset_column_information
+
+    Problem.find(:all).each do |problem|
+      problem.test_allowed = true
+      problem.save
+    end
+  end
+
+  def self.down
+    remove_column :problems, :test_allowed
+  end
+end
