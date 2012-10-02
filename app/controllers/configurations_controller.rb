48e0df53a2ed0a408b3de6e4e0cc0998efb8362d
Jittat Fakcharoenphol <jittat@gmail.com>
2012-10-02 15:32:42 +0700

    fixed in_place_editor for configs, updated form in users/new

--- app/controllers/configurations_controller.rb
+++ app/controllers/configurations_controller.rb
@@ -3,9 +3,9 @@ class ConfigurationsController < ApplicationController
   before_filter :authenticate
   before_filter { |controller| controller.authorization_by_roles(['admin'])}
 
-  in_place_edit_for :configuration, :key
-  in_place_edit_for :configuration, :type
-  in_place_edit_for :configuration, :value
+  in_place_edit_for :grader_configuration, :key
+  in_place_edit_for :grader_configuration, :type
+  in_place_edit_for :grader_configuration, :value
 
   def index
     @configurations = GraderConfiguration.find(:all,
