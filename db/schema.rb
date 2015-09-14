fd595d6420baa23039694de00315cee2a8712fca
Nattee Niparnan <nattee@gmail.com>
2015-09-15 05:36:20 +0700

    add license agreement checkbox
    --HG--
    branch : java

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150914155101) do
+ActiveRecord::Schema.define(:version => 20150914223258) do
 
   create_table "announcements", :force => true do |t|
     t.string   "author"
