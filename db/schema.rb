65abb9869cfd4b849b82487f0d06caa6df9e1015
Nattee Niparnan <nattee@gmail.com>
2016-10-31 16:21:54 +0700

    update options
    --HG--
    branch : java

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20161014091417) do
+ActiveRecord::Schema.define(:version => 20161031063337) do
 
   create_table "announcements", :force => true do |t|
     t.string   "author"
